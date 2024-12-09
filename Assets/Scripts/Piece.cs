4d937c95cb1bae72842580143f8ec739df09bd15
mw2358006 <2358006@mokwon.ac.kr>
2024-12-09 17:26:48 +0900

    코드를 이븐하게 했음

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -151,8 +151,8 @@ public class Piece : MonoBehaviour
           cell.y -= 0.5f;
           x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
           y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
-
           break;
+
         default:
           x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
           y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
